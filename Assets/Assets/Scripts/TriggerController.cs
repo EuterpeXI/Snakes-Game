3227d6b81d7e25ed5491fa0a585934ee448a6dbe
Teagan <Teagan@DESKTOP-HGDQKH8>
2018-11-01 22:06:22 -0600

    Added Tilesets, simple snake shooting

--- Assets/Assets/Scripts/TriggerController.cs
+++ Assets/Assets/Scripts/TriggerController.cs
@@ -5,15 +5,40 @@ using UnityEngine;
 public class TriggerController : MonoBehaviour 
 {
 	public GameObject door;
-	
+	public float gracePeriod = 0.0f;
+	public bool startsOpen = false;
+
+	private bool isOpen;
+	private float timeClosed;
+
+	void Start()
+	{
+		isOpen = startsOpen;
+		timeClosed = -gracePeriod;
+	}
+
+	void Update()
+	{
+		if (Time.time - timeClosed <= gracePeriod) 
+		{
+			door.SetActive (isOpen);
+		}
+		else
+		{
+			door.SetActive (!isOpen);
+		}
+
+	}
+
 	void OnTriggerStay2D()
 	{
-			door.SetActive(false);
+		isOpen = !startsOpen;
 	}
 	
 		void OnTriggerExit2D()
 	{
-			door.SetActive(true);
+		isOpen = startsOpen;
+		timeClosed = Time.time;
 	}
 
 }
