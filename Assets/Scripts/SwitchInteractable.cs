b7c56002d0dd3aa641245a3e1e7fc77cd7d9ea83
Amelia Chin <amelia.chin@live.ca>
2019-03-14 12:37:39 -0600

    Updates to the switch trigger event

--- Assets/Scripts/SwitchInteractable.cs
+++ Assets/Scripts/SwitchInteractable.cs
@@ -1,21 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
+using UnityEditor;
 
 public class SwitchInteractable : MonoBehaviour
 {
 
-    // Update is called once per frame
-    void Update()
+    bool OnTriggerStay2D(Collider2D other)
     {
-        
-    }
-
-    void OnCollisionStay(Collision collision)
-    {
-        foreach(ContactPoint contact in collision.contacts)
-        {
-            // Keep the door open
+        if(other.tag == "snake_bullet"){
+            // Have the script for the doors handle the conditions
+            return true;
         }
+        
+        return false;
     }
 }
