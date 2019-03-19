716b4d6f8b6adfea159f6dc51bfb3e65bc54f3a1
Teagan <Teagan@DESKTOP-HGDQKH8>
2019-03-19 14:00:24 -0600

    Adds android build support, working joystick and mutlitouch controls, double click to fire. Properly integrates TopDownPlayerController. Nukes Scripts/PrefabBrush.cs as it breaks the build as well as some old unneccesary code. Note: I added platform dependent code (all in InputManager) don't know if that is how I should do that in C#

--- Assets/Scripts/LaserPointController.cs
+++ Assets/Scripts/LaserPointController.cs
@@ -4,9 +4,9 @@ public class LaserPointController : MonoBehaviour
 {
     public static Vector3 position;
 
-    void Update()
+    public void MoveLaser(Vector3 location)
     {
-        transform.position = (Vector2) Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        transform.position = (Vector2)Camera.main.ScreenToWorldPoint(location);
         position = transform.position;
     }
 }
--- Assets/Scripts/MobileJoystickPlayerController.cs
+++ Assets/Scripts/MobileJoystickPlayerController.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 
 public class MobileJoystickPlayerController : MonoBehaviour
 {
-    public TopDownMovementController player;
     
     [Tooltip("this is what relative will calculate")]
     public Transform center;
@@ -12,34 +11,48 @@ public class MobileJoystickPlayerController : MonoBehaviour
     [Tooltip("Moves with the input position")]
     public Transform innerCircle;
 
+    public float joystickRadius = 40.0f;
 
-    void moveCharacter(Vector3 direction)
+    public Vector2 OnJoystickDrag(Vector3 dragPosition)
     {
-        direction.z = 0; //to not interfere  with normalization.
-        direction = direction.normalized;
-
-        Debug.Log(direction, this);
-        player.Move(-direction.x * Time.deltaTime,
-                    -direction.y * Time.deltaTime);
-        //POTENTIAL BUG if it does not go in right direction adjust here
-        // -> depends on orientation of screen ->
+        Vector3 moveDirection = Vector3.ClampMagnitude(dragPosition - center.position, joystickRadius);
+        innerCircle.position = center.position + moveDirection;
+        return moveDirection / joystickRadius;
+    }
+    public Vector2 ResetJoystick()
+    {
+        return OnJoystickDrag(center.position);
     }
 
-// Note this won't stop dragging/ pdating once you start dragging and leave the collider. -> good 
-    void OnMouseDrag()
-    {
-        Debug.Log("Mouse Drag");
-        Vector3 input = getMousePosition();
-        innerCircle.position = input;
+    /*
+        void moveCharacter(Vector3 direction)
+        {
+            direction.z = 0; //to not interfere  with normalization.
+            direction = direction.normalized;
 
-        moveCharacter(center.position - input);
-        
-    }
+            Debug.Log(direction, this);
+            player.Move(-direction.x * Time.deltaTime,
+                        -direction.y * Time.deltaTime);
+            //POTENTIAL BUG if it does not go in right direction adjust here
+            // -> depends on orientation of screen ->
+        }
 
-    Vector2 getMousePosition()
-    {
-        //yes it does need to be vector2 ->  strips z axis so that setting position does not change
-        return Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        // Note this won't stop dragging/ pdating once you start dragging and leave the collider. -> good 
+        void OnMouseDrag()
+        {
+            Debug.Log("Mouse Drag");
+            Vector3 input = getMousePosition();
+            innerCircle.position = input;
 
-    }
+            moveCharacter(center.position - input);
+
+        }
+
+        Vector2 getMousePosition()
+        {
+            //yes it does need to be vector2 ->  strips z axis so that setting position does not change
+            return Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+        }
+    */
 }
