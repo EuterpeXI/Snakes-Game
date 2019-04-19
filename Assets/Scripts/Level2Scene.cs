b59e9c25bf5400b71f826b1ade0124c1598472b2
Amelia Chin <amelia.chin@live.ca>
2019-04-18 23:54:54 -0600

    SceneManagement changes added.

---
+++ Assets/Scripts/Level2Scene.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Level2Scene : MonoBehaviour
+{
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        Application.LoadLevel("Level 2");
+    }
+}
