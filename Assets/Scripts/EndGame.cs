8d6bff83cc7a99549a1e24785a81c1fd15c29642
Amelia Chin <amelia.chin@live.ca>
2019-04-19 23:44:24 -0600

    Updates to level 3 and included APK file

---
+++ Assets/Scripts/EndGame.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndGame : MonoBehaviour
+{
+    public GameObject panel;
+
+    private void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.tag == "Player"){
+            panel.gameObject.SetActive(true);
+        }
+        else{
+            panel.gameObject.SetActive(false);
+        }
+    }
+}
